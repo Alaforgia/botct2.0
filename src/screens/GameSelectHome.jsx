89e664220b518a033cd84d8cf5cce74b90fe6ef4
Tony LaForgia <alforgia428@gmail.com>
2023-07-13 22:34:52 -0500

    empty test files

---
+++ src/screens/GameSelectHome.jsx
@@ -0,0 +1,16 @@
+function GameSelectHome() {
+  return (
+    <>
+    <div className="flex flex-col items-center justify-center">
+    <h2 className="p-6 mb-5 text-3xl font-bold underline">BOTCT DEMO</h2>
+    <p className="p-1 mb-4 font-serif font-bold text-4xl">Select Game Type:</p>
+    <div className="p-2 font-serif font-bold text-3xl">
+      <p className="p-1 mb-7">Trouble Brewing</p>
+      <p className="p-1 mb-7">Coming Soon...</p>
+    </div>
+  </div>
+    </>
+  )
+}
+
+export default GameSelectHome
