50868b96d4b970b296e205e7a8f90ec336918429
Tony LaForgia <alforgia428@gmail.com>
2023-07-13 22:10:21 -0500

    basic styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import "./App.css";
+
 
 function App() {
   return (
     <>
-      <h2 className="pb-10 text-3xl font-bold underline">BOTCT DEMO</h2>
       <div className="flex flex-col items-center justify-center">
-        <p className="pb-3 mb-7 font-serif font-bold text-4xl">Select Game Type:</p>
-        <div className="pb-5 font-serif font-bold text-3xl">
-          <p className="pb-9">Trouble Brewing</p>
-          <p>Coming Soon...</p>
+        <h2 className="p-6 mb-5 text-3xl font-bold underline">BOTCT DEMO</h2>
+        <p className="p-1 mb-4 font-serif font-bold text-4xl">Select Game Type:</p>
+        <div className="p-2 font-serif font-bold text-3xl">
+          <p className="p-1 mb-7">Trouble Brewing</p>
+          <p className="p-1 mb-7">Coming Soon...</p>
         </div>
       </div>
     </>
