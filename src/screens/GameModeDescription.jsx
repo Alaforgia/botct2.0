44c62e32fddef64fb2547c2d04f701c7b45d1437
Tony LaForgia <alforgia428@gmail.com>
2023-07-14 21:28:54 -0500

    back button route

--- src/screens/GameModeDescription.jsx
+++ src/screens/GameModeDescription.jsx
@@ -1,9 +1,19 @@
-
+import { Outlet, Link } from "react-router-dom";
 
 function GameModeDescription() {
+  // Trouble Brewing description static data will go here
   return (
-    <div>GameModeDescription</div>
-  )
+    <>
+      <div>
+        GameModeDescription
+        <button>Continue To Set-Up</button>
+        <Link to={"/"}>
+          <p>Back</p>
+        </Link>
+        <Outlet />
+      </div>
+    </>
+  );
 }
 
-export default GameModeDescription
+export default GameModeDescription;
