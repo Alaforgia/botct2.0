0cea041c8ddcd119a8cebc98a725f00c681f027f
Tony LaForgia <alforgia428@gmail.com>
2023-07-14 19:39:31 -0500

    rrd set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-// import { Route } from "react-router-dom";
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from "react-router-dom";
 import GameSelectHome from "./screens/GameSelectHome";
+
+const router = createBrowserRouter(createRoutesFromElements(<Route path="/" element={<GameSelectHome />}>
+</Route>));
 
 function App() {
   return (
     <>
-      {/* <Route> */}
-        <GameSelectHome />
-      {/* </Route> */}
+      <RouterProvider router={router} />
     </>
   );
 }
--- src/screens/GameSelectHome.jsx
+++ src/screens/GameSelectHome.jsx
@@ -1,16 +1,25 @@
+import { Outlet, Link } from "react-router-dom";
+
 function GameSelectHome() {
+  const handleGameSelectClick = () => {};
   return (
     <>
-    <div className="flex flex-col items-center justify-center">
-    <h2 className="p-6 mb-5 text-3xl font-bold underline">BOTCT DEMO</h2>
-    <p className="p-1 mb-4 font-serif font-bold text-4xl">Select Game Type:</p>
-    <div className="p-2 font-serif font-bold text-3xl">
-      <p className="p-1 mb-7">Trouble Brewing</p>
-      <p className="p-1 mb-7">Coming Soon...</p>
-    </div>
-  </div>
+      <div className="flex flex-col items-center justify-center">
+        <h2 className="p-6 mb-5 text-3xl font-bold underline">BOTCT DEMO</h2>
+        <p className="p-1 mb-4 font-serif font-bold text-4xl ">Select Game Type:</p>
+        <div className="p-2 font-serif font-bold text-3xl">
+          <p
+            className="p-3 mb-7 border-spacing-6 border-2 border-black rounded-xl cursor-pointer"
+            onClick={handleGameSelectClick()}
+          >
+            <Link to="/">Trouble Brewing</Link>
+          </p>
+          <p className="p-1 mb-7">Coming Soon...</p>
+        </div>
+      </div>
+      <Outlet />
     </>
-  )
+  );
 }
 
-export default GameSelectHome
+export default GameSelectHome;
