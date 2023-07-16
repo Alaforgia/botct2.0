fa6e5d3f1c8954c04c0630de4b1574bad06a2f8e
Tony LaForgia <alforgia428@gmail.com>
2023-07-16 17:40:19 -0500

    states compo

---
+++ src/data/RuleBookData/SetupData.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function SetupData() {
+  const gatherPLayers = `Get one chair per player and arrange the chairs facing each other. A rough circle or square is fine, as long as people are sitting in a definite clockwise or counterclockwise order. As the Storyteller, you will need to be able to enter and exit the circle often, so leave a gap between two chairs. The center of this space will need to be mostly empty - no tables or hazards on the floor which can be tripped on.
+  `;
+
+  
+  return <div>SetupData</div>;
+}
+
+export default SetupData;
