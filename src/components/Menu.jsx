6eac4d2ca3e0c7f890a7e81ee0c4bacf581bf9b8
Tony LaForgia <alforgia428@gmail.com>
2023-07-16 17:22:27 -0500

    menu component

---
+++ src/components/Menu.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Menu() {
+  //TODO: This will contain all the components and data for the rules. It needs to be accessible in every screen w/o effecting
+  //TODO: the state or cause a refresh. Accessing the rules is extremely important for the game.
+  return <div>Menu</div>;
+}
+
+export default Menu;
