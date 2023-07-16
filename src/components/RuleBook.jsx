247c7ef2ce1d5117dfc3696838b2141468d5ccfb
Tony LaForgia <alforgia428@gmail.com>
2023-07-16 17:22:22 -0500

    rulebook component

--- src/components/RuleBook.jsx
+++ src/components/RuleBook.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 function RuleBook() {
-  return <div>RuleBook</div>;
+  return (
+    <>
+      <div>RuleBook</div>;
+      <div>
+        <p>Setup</p>
+        <p>Rules</p>
+        <p>Abilities</p>
+        <p>States</p>
+      </div>
+    </>
+  );
 }
 
 export default RuleBook;
