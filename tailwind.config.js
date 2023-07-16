1bf51ab4147a61fdd66a983fc2466d657783dc11
Tony LaForgia <alforgia428@gmail.com>
2023-07-16 16:44:40 -0500

    custom css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", "*"],
   theme: {
     extend: {
       fontFamily: {
         display: ["Ysabeau SC", "sans-serif"],
       },
+      backgroundColor: { "neu-button": "linear-gradient(145deg, #9eaec5, #636d7b)" },
+      borderRadius: { "neu-button": "72px" },
+      boxShadow: { "neu-button": "13px 13px 26px #727e8e,-13px -13px 26px #a6b7ce" },
     },
+
+    plugins: ["tailwindcss-neumorphism"],
   },
-  plugins: [],
 };
